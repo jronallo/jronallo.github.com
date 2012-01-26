e37e57d19de3cb23b46125ce405693968db5b5f2
Jason Ronallo <jronallo@gmail.com>
2012-01-26 12:12:10 -0500

    comment out console.log

--- source/javascripts/pin.js
+++ source/javascripts/pin.js
@@ -10,14 +10,14 @@ jQuery(document).ready(function() {
     });
     base_url = 'http://feeds.pinboard.in/json/v1/u:jronallo' + tags_in_url;
     url =  base_url + "?count=2&cb=?";
-    console.log(url);
+    //console.log(url);
     jQuery.ajax( { 
       type: "GET",
       url: url,
       dataType: "jsonp",
       success: function(json) {
         var ul = jQuery(document.createElement('ul'));     
-        console.log(json);   
+        //console.log(json);   
         jQuery.each(json, function(index, item){
           var string = '<li><a href="'+ item.u + '">' + item.d + '</a><br/> ' +  item.n + '</li>';
           jQuery(ul).append( string );
@@ -28,7 +28,7 @@ jQuery(document).ready(function() {
         $(that).append(see_all_link);
       },
       error: function(x,y,z) {
-        console.log(x,y,z);
+        //console.log(x,y,z);
       }
       
     });
